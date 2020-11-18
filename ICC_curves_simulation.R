37f22045fd0559c5da2fa189237ebea0eea7bd08
Diego Figueiras <figueirasd1@montclair.edu>
2020-11-18 15:47:05 -0500

    plots with CTT parameters

--- ICC_curves_simulation.R
+++ ICC_curves_simulation.R
@@ -5,7 +5,7 @@ exam<- data.frame(item1=sample(0:1,prob=c(0.6,0.4),6000,replace=TRUE),
                   item4=sample(0:1,prob=c(0.9,0.1),6000,replace=TRUE),
                   item5=sample(0:1,prob=c(0.4,0.6),6000,replace=TRUE),
                   item6=sample(0:1,prob=c(0.2,0.8),6000,replace=TRUE)
-                  )
+)
 
 # dif<- difGenLogistic(exam, group="group", focal.name="A")
 # plot(dif, type="trace", face_items=FALSE)
@@ -24,17 +24,38 @@ plot(results, type='trace',face_items=FALSE)
 
 ## CTT METHOD
 
-exam$total<-rowMeans(exam)
-exam$zscore<-scale(exam$total)
-logreg<-glm(item1~ total, data=exam, family="binomial")
-summary(logreg)
-prob<-data.frame(logreg$coefficients)
+# exam$total<-rowMeans(exam)
+# exam$zscore<-scale(exam$total)
+# logreg<-glm(item1~ total, data=exam, family="binomial")
+# summary(logreg)
+# prob<-data.frame(logreg$coefficients)
+# plot(exam$total, exam$item1)
+# curve(predict(logreg,data.frame(total=x),type="resp"),add=TRUE)
+alpha(exam)
+## For item1
+total<-rowMeans(exam)
+p<-mean(exam$item1)
+a<- -0.0044
+x <- 1/(1+(exp(-a*(total-p))))
+plot(total,x, type="l")
 
-plot(exam$total, exam$item1)
-curve(predict(logreg,data.frame(total=x),type="resp"),add=TRUE)
+## For item5
+total<-rowMeans(exam)
+p<-mean(exam$item5)
+a<- 0.0014
+x <- 1/(1+(exp(-a*(total-p))))
+plot(total,x, type="l")
 
+## For item4 (high discrimination in IRT)
+total<-rowMeans(exam)
+p<-mean(exam$item4)
+a<- 0.0091
+x <- 1/(1+(exp(-a*(total-p))))
+plot(total,x, type="l")
 
-logreg2<-glm(item5~ total, data=exam, family="binomial")
-summary(logreg)
-plot(exam$total, exam$item5)
-curve(predict(logreg2,data.frame(total=x),type="resp"),add=TRUE)
+#For item2
+total<-rowMeans(exam)
+p<-mean(exam$item2)
+a<- -0.0064
+x <- 1/(1+(exp(-a*(total-p))))
+plot(total,x, type="l")
